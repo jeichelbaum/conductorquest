3a2d21e53476b49a279dd5e861265fade62299c3
eichelbaum <trash.meil@gmx.de>
2017-01-21 05:00:34 +0100

    added sounds and simple sound controller

---
+++ Assets/scripts/controller/battle/BeatController.cs
@@ -0,0 +1,133 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeatController : MonoBehaviour
+{
+    public static BeatController instance;
+
+    public delegate void PatternTick();
+    public event PatternTick OnPatternTick;
+
+    public delegate void TickUpdate();
+    public event TickUpdate OnTickUpdate;
+    public delegate void BeatUpdate();
+    public event BeatUpdate OnBeatUpdate;
+    public delegate void BarUpdate();
+    public event BarUpdate OnBarUpdate;
+
+    public float bpm = 120f;
+    float beatInterval = 0f;
+    float tickInterval = 0f;
+
+    float time = 0f;
+    public int tick = -1;
+    public int beat = -1;
+    public int bar = -1;
+    public bool isPlaying = false;
+    
+    List<int> r1 = new List<int>(new int[] { 0, 4, 8, 12 });
+    List<int> r2 = new List<int>(new int[] { 0, 4, 10, 14 });
+    List<int> r3 = new List<int>(new int[] { 0, 4, 6, 8, 12 });
+    List<int> r4 = new List<int>(new int[] { 0, 4, 7, 10, 14 });
+    List<int> r5 = new List<int>(new int[] { 0, 6, 7, 10, 14 });
+
+    List<int>[] patterns;
+    List<int> pattern;
+
+    void Awake()
+    {
+        beatInterval = 60f / bpm;
+        tickInterval = beatInterval / 4f;
+
+        patterns = new List<int>[] { r1, r2, r3, r4, r5 };
+        SelectPatterRandom();
+
+        instance = this;
+    }
+
+    void FixedUpdate()
+    {
+        UpdateTime();
+    }
+
+    void UpdateTime()
+    {
+        if (!isPlaying) return;
+        time += Time.deltaTime;
+
+        if (time > tickInterval) // TICK
+        {
+            time -= tickInterval;
+            tick = (tick + 1) % 16;
+            OnTick();
+
+            if (tick % 4 == 0) // BEAT
+            {
+                beat = (beat + 1) % 4;
+                OnBeat();
+
+                if (beat == 0) // BAR
+                {
+                    bar++;
+                    OnBar();
+                }
+            }
+        }
+    }
+
+    void OnTick()
+    {
+        if (OnTickUpdate != null)
+        {
+            OnTickUpdate();
+        }
+
+        if (pattern.Contains(tick))
+        {
+            if (OnPatternTick != null)
+            {
+                OnPatternTick();
+            }
+        }
+    }
+
+    void OnBeat()
+    {
+        if (OnBeatUpdate != null)
+        {
+            OnBeatUpdate();
+        }
+    }
+
+    void OnBar()
+    {
+        if(OnBarUpdate != null)
+        {
+            OnBarUpdate();
+        }
+    }
+
+    public void StartPlaying()
+    {
+        isPlaying = true;
+    }
+
+    public void StopPlaying()
+    {
+        time = 0f;
+        tick = bar = beat = -1;
+        isPlaying = false;
+    }
+
+    public void SelectPattern(int index)
+    {
+        pattern = patterns[index];
+    }
+
+    public void SelectPatterRandom()
+    {
+        var p = Random.Range(0, patterns.Length);
+        pattern = patterns[p];
+    }
+}
---
+++ Assets/scripts/view/SoundManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour {
+
+    public static SoundManager instance;
+
+    public AudioClip switch_enemy;
+    public AudioClip[] monster_sound;
+    public AudioClip sword_draw;
+    public AudioClip[] sword_slash;
+    public AudioClip slash_fail;
+
+    void Awake()
+    {
+        instance = this;
+    }
+
+    void PlayClip(AudioClip clip)
+    {
+        AudioSource.PlayClipAtPoint(clip, Camera.main.transform.position);
+    }
+
+    public void PlaySwitchEnemy()
+    {
+        PlayClip(switch_enemy);
+    }
+
+    public void PlayMonsterSound(int index)
+    {
+        PlayClip(monster_sound[index]);
+    }
+
+    public void PlaySwordCraw()
+    {
+        PlayClip(sword_draw);
+    }
+
+    public void PlaySwordSlash(int index)
+    {
+        PlayClip(sword_slash[index]);
+    }
+    
+    public void PlaySlashFail()
+    {
+        PlayClip(slash_fail);
+    }
+}
