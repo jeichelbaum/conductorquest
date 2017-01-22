4bf83508f952af336fc54262b0dbc269bb1148af
eichelbaum <trash.meil@gmx.de>
2017-01-22 16:32:45 +0100

    added random backgrounds

--- Assets/scripts/view/battle/BackgroundView.cs
+++ Assets/scripts/view/battle/BackgroundView.cs
@@ -4,17 +4,42 @@ using UnityEngine;
 
 public class BackgroundView : MonoBehaviour {
 
-    string anim_switch = "background_switch";
+    static string anim_idle = "background_idle";
+    static string anim_switch = "background_switch";
 
     Animator animator;
+    bool switching = false;
+    float t_switching = 0f;
 
-	void Start ()
+    public Sprite[] backgrounds;
+    public SpriteRenderer bg_left;
+    public SpriteRenderer bg_right;
+
+
+    void Start ()
     {
         animator = GetComponent<Animator>();
 	}
+
+    void Update()
+    {
+        if(switching && t_switching + 0.1f < Time.time)
+        {
+            Debug.Log("check switch");
+            if(animator.GetCurrentAnimatorStateInfo(0).IsName(anim_idle))
+            {
+                bg_left.sprite = bg_right.sprite;
+                switching = false;
+            }
+        }
+    }
 	
     public void PlaySwitchAnimation()
     {
         animator.Play(anim_switch);
+        switching = true;
+        t_switching = Time.time;
+
+        bg_right.sprite = backgrounds[Random.Range(0, backgrounds.Length)];
     }
 }
