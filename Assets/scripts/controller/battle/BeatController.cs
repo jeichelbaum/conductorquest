e27be2d2248ca706a9b6ea2fda63a1214a8e7cc5
eichelbaum <trash.meil@gmx.de>
2017-01-22 17:03:44 +0100

    added beats

--- Assets/scripts/controller/battle/BeatController.cs
+++ Assets/scripts/controller/battle/BeatController.cs
@@ -31,11 +31,39 @@ public class BeatController : MonoBehaviour
     public int beat = -1;
     public int bar = -1;
     public bool isPlaying = false;
+
+    // only 4ths
+    List<int> r1_1 = new List<int>(new int[] { 0, 4, 8, 16, 20, 24 });
+    List<int> r1_2 = new List<int>(new int[] { 0, 4, 8, 12, 16, 20, 24 });
+    List<int> r1_3 = new List<int>(new int[] { 0, 4, 8, 12, 16, 20, 24 });
+    List<int> r1_4 = new List<int>(new int[] { 0, 4, 12, 16, 20, 24 });
+    List<int> r1_5 = new List<int>(new int[] { 0, 8, 12, 16, 24 });
+
+    // only 4th double time
+    List<int> r2_1 = new List<int>(new int[] { 0, 4, 6, 8, 12, 16, 18, 20, 24 });
+    List<int> r2_2 = new List<int>(new int[] { 0, 2, 4, 8, 10, 12, 16, 18, 20, 24 });
+    List<int> r2_3 = new List<int>(new int[] { 0, 4, 6, 8, 10, 12, 16, 18, 20, 22, 24 });
     
-    List<int> r1 = new List<int>(new int[] { 0, 4, 8, 12, 16, 20, 24 });
-    List<int> r3 = new List<int>(new int[] { 0, 4, 6, 8, 12, 16, 18, 20, 24 });
-    List<int> r2 = new List<int>(new int[] { 0, 4, 10, 14, 20, 24 });
-    List<int> r4 = new List<int>(new int[] { 0, 4, 7, 10, 16, 20, 23, 26 });
+    // offbeat #1
+    List<int> r3_1 = new List<int>(new int[] { 0, 4, 10, 14, 20, 24 });
+    List<int> r3_2 = new List<int>(new int[] { 0, 4, 10, 14, 16, 20, 24 });
+    List<int> r3_3 = new List<int>(new int[] { 0, 4, 10, 14, 20, 24 });
+    List<int> r3_4 = new List<int>(new int[] { 0, 4, 10, 14, 20, 24 });
+
+    // offbeat #2
+    List<int> r4_1 = new List<int>(new int[] { 0, 4, 7, 10, 16, 20, 23, 26 });
+    List<int> r4_2 = new List<int>(new int[] { 0, 4, 7, 10, 15, 18, 21, 23 });
+
+    // offbeat #3
+    List<int> r5_1 = new List<int>(new int[] { 0, 3, 4, 10, 14, 16, 19, 20, 26});
+    List<int> r5_2 = new List<int>(new int[] { 0, 3, 4, 8, 12, 14, 19, 20, 26});
+
+    List<int> r6_1 = new List<int>(new int[] { 0, 6, 12, 18, 24 });
+    List<int> r6_2 = new List<int>(new int[] { 0, 6, 20, 24 });
+    List<int> r6_3 = new List<int>(new int[] { 0, 9, 15, 24 });
+    List<int> r6_4 = new List<int>(new int[] { 0, 2, 19 });
+
+    public bool easyMode = true;
 
     int patternIndex = -1;
     List<int>[] patterns;
@@ -46,7 +74,22 @@ public class BeatController : MonoBehaviour
         beatInterval = 60f / bpm;
         tickInterval = beatInterval / 4f;
 
-        patterns = new List<int>[] { r1, r2, r3, r4 };
+        patterns = new List<int>[] {
+            r1_1, r1_2, r1_3, r1_4, r1_5,
+            r2_1, r2_2, r2_3,
+            r3_1, r3_2, r3_3, r3_4,
+            r4_1, r4_2,
+            r5_1, r5_2,
+            r6_1, r6_2, r6_3, r6_4
+        };
+
+        if (easyMode)
+        {
+            patterns = new List<int>[] {
+                r1_1, r1_2, r1_3, r1_4, r1_5,
+                r2_1, r2_2, r2_3
+            };
+        }
 
         instance = this;
     }
